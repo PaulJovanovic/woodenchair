762b9ae8f44e03496436f3d9309c28106bef2061
Paul <Pauldrik@comcast.net>
2013-05-05 11:33:39 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Woodenchair::Application.config.secret_token = '12b0fb63d2a038e356eadb4e5376a3ca1bd6ea0d6bb35e4c67d1f611b5fab2d862793dd018dc071dabf0c34f4370643d02f1b67962291ba0915e226d375c8d29'
